d8277be1068d91b236019ab058f9c82e0b1195c5
EchoBubble <1756865212@qq.com>
2026-06-29 11:11:40 +0800

    Lag Compensation Component

---
+++ Source/Blaster/Private/BlasterComponent/LagCompensationComponent.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlasterComponent/LagCompensationComponent.h"
+
+
+ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+
+}
+
+void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                              FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+
+}
+
